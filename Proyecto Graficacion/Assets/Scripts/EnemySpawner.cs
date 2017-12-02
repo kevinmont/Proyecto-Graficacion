4dceb68baf9d0b11fcc2a984a86c6086a0cef4e8
JulioPerezAG <julioperezag@gmail.com>
2017-12-02 13:18:41 -0600

    Se inicio el repo

---
+++ Proyecto Graficacion/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Networking;
+
+public class EnemySpawner : NetworkBehaviour {
+
+	public GameObject enemyPrefab;
+	public int numEnemies;
+		
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	 public override void OnStartServer() {
+        for (int i=0; i < numEnemies; i++) {
+            var pos = new Vector3(
+                Random.Range(-8.0f, 8.0f),
+                0.2f,
+                Random.Range(-8.0f, 8.0f)
+                );
+
+            var rotation = Quaternion.Euler( Random.Range(0,180), Random.Range(0,180), Random.Range(0,180));
+
+            var enemy = (GameObject)Instantiate(enemyPrefab, pos, rotation);
+            NetworkServer.Spawn(enemy);
+        }
+    }
+}
---
+++ Proyecto Graficacion/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour {
+
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer)
+			return;
+
+		var x = Input.GetAxis ("Horizontal") * 0.1f;
+		var z = Input.GetAxis ("Vertical") * 0.1f;
+
+		transform.Translate (x, 0, z);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			CmdFire ();
+		}
+	}
+
+	public override void OnStartLocalPlayer() {
+		GetComponent<MeshRenderer> ().material.color = Color.blue;
+	}
+
+	[Command]
+	private void CmdFire() {
+		GameObject bullet = (GameObject)Instantiate (bulletPrefab, transform.position - transform.forward, Quaternion.identity);
+
+		bullet.GetComponent<Rigidbody> ().velocity = -transform.forward * 4;
+
+		NetworkServer.Spawn (bullet);
+
+		Destroy (bullet, 2f);
+	}
+}
